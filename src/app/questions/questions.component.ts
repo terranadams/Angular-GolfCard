120fbeeb61162442efcbaa86d2530f82f2109922
Terran Adams <terranadams@Terrans-MBP.veracitynetworks.net>
2021-04-01 09:55:27 -0600

    getting user input

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -8,17 +8,39 @@ import { CoursesService } from '../../services/courses.service'
   styleUrls: ['./questions.component.css']
 })
 export class QuestionsComponent implements OnInit {
+
+  courseGood: boolean = true
+  playerCountGood: boolean = false
+  difficultyGood: boolean = false
   
-  courseID: number
+  courseID: string
+  playerCount: string
+  difficulty: string
+
+  begin: boolean = false
 
   selectCourse(): void {
-    // console.log(this.courseID)
-    
+    this.courseGood = false
+    this.playerCountGood = true
+
+  }
+
+  numOfPlayers(): void {
+    this.playerCountGood = false
+    this.difficultyGood = true
+  }
+
+  selectDiff(): void {
+    this.difficultyGood = false
+    this.begin = true
+  }
+
+  start(): void {
+    console.log(this.courseID, this.playerCount, this.difficulty)
   }
 
   constructor(private coursesService: CoursesService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
