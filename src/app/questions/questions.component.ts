16c5df01feb6a0b2d5ab02d4918e76721424c106
Terran Adams <terranadams@Terrans-MBP.veracitynetworks.net>
2021-04-04 11:54:25 -0600

    stuck

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -10,34 +10,44 @@ import { CoursesService } from '../../services/courses.service'
 export class QuestionsComponent implements OnInit {
 
   courseGood: boolean = true
-  playerCountGood: boolean = false
   difficultyGood: boolean = false
+  playerCountGood: boolean = false
+  namesGood: boolean = false
   
   courseID: string
-  playerCount: string
+  playerCount: number
   difficulty: string
+  playerCountArray: number[] = []
 
   begin: boolean = false
 
-  selectCourse(): void {
-    this.courseGood = false
-    this.playerCountGood = true
-
+  arrayMaker(n: number): void {
+    if (this.playerCountArray.length > n) {
+      this.playerCountArray.push(n)
+    } 
   }
 
-  numOfPlayers(): void {
-    this.playerCountGood = false
+  selectCourse(): void {
+    this.courseGood = false
     this.difficultyGood = true
   }
 
   selectDiff(): void {
     this.difficultyGood = false
-    this.begin = true
+    this.playerCountGood = true
   }
 
-  start(): void {
-    console.log(this.courseID, this.playerCount, this.difficulty)
+  numOfPlayers(): void {
+    this.difficultyGood = false
+    this.namesGood = true
+
+    this.arrayMaker(this.playerCount)
   }
+
+  console(): void{
+    console.log(this.playerCount, this.playerCount + 2)
+  }
+
 
   constructor(private coursesService: CoursesService) { }
 
