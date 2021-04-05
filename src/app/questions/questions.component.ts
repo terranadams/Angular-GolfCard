3bbc997e1655200a0c8ddadd833efc08d56e3fc8
Terran Adams <terranadams@Terrans-MBP.veracitynetworks.net>
2021-04-05 12:34:01 -0600

    inputs working now

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -2,60 +2,140 @@ import { Component, OnInit } from '@angular/core';
 import { CoursesService } from '../../services/courses.service';
 import { Player } from '../interfaces/player';
 
-
-
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
-  styleUrls: ['./questions.component.css']
+  styleUrls: ['./questions.component.css'],
 })
 export class QuestionsComponent implements OnInit {
+  courseRender: boolean = true;
+  difficultyRender: boolean = false;
+  playerCountRender: boolean = false;
+  nameAskRender: boolean = false;
 
-  courseRender: boolean = true
-  difficultyRender: boolean = false
-  playerCountRender: boolean = false
-  nameAskRender: boolean = false
-  
-  courseID: string
-  playerCount: number
-  difficulty: string
-  playerCountArray: number[] = []
+  courseID: string;
+  playerCount: number;
+  difficulty: string;
+  playerCountArray: number[] = [];
 
-  begin: boolean = false
+  begin: boolean = false;
 
-  
+  playersArray: Player[] = [
+    {
+      name: '',
+      hole1: 0,
+      hole2: 0,
+      hole3: 0,
+      hole4: 0,
+      hole5: 0,
+      hole6: 0,
+      hole7: 0,
+      hole8: 0,
+      hole9: 0,
+      hole10: 0,
+      hole11: 0,
+      hole12: 0,
+      hole13: 0,
+      hole14: 0,
+      hole15: 0,
+      hole16: 0,
+      hole17: 0,
+      hole18: 0,
+    },
+    {
+      name: '',
+      hole1: 0,
+      hole2: 0,
+      hole3: 0,
+      hole4: 0,
+      hole5: 0,
+      hole6: 0,
+      hole7: 0,
+      hole8: 0,
+      hole9: 0,
+      hole10: 0,
+      hole11: 0,
+      hole12: 0,
+      hole13: 0,
+      hole14: 0,
+      hole15: 0,
+      hole16: 0,
+      hole17: 0,
+      hole18: 0,
+    },
+    {
+      name: '',
+      hole1: 0,
+      hole2: 0,
+      hole3: 0,
+      hole4: 0,
+      hole5: 0,
+      hole6: 0,
+      hole7: 0,
+      hole8: 0,
+      hole9: 0,
+      hole10: 0,
+      hole11: 0,
+      hole12: 0,
+      hole13: 0,
+      hole14: 0,
+      hole15: 0,
+      hole16: 0,
+      hole17: 0,
+      hole18: 0,
+    },
+    {
+      name: '',
+      hole1: 0,
+      hole2: 0,
+      hole3: 0,
+      hole4: 0,
+      hole5: 0,
+      hole6: 0,
+      hole7: 0,
+      hole8: 0,
+      hole9: 0,
+      hole10: 0,
+      hole11: 0,
+      hole12: 0,
+      hole13: 0,
+      hole14: 0,
+      hole15: 0,
+      hole16: 0,
+      hole17: 0,
+      hole18: 0,
+    },
+  ];
 
   selectCourse(): void {
-    this.courseRender = false
-    this.difficultyRender = true
+    this.courseRender = false;
+    this.difficultyRender = true;
   }
 
   selectDiff(): void {
-    this.difficultyRender = false
-    this.playerCountRender = true
+    this.difficultyRender = false;
+    this.playerCountRender = true;
   }
 
   numOfPlayers(): void {
-    this.playerCountRender = false
-    this.difficultyRender = false
-    this.nameAskRender = true
+    this.playerCountRender = false;
+    this.difficultyRender = false;
+    this.nameAskRender = true;
 
-    this.arrayMaker(this.playerCount)
+    this.inputMaker(this.playerCount);
   }
 
-  arrayMaker(n: number): void {
+  inputMaker(n: number): void {
     while (this.playerCountArray.length < n) {
-      this.playerCountArray.push(n)
-    } 
+      this.playerCountArray.push(n);
+    }
   }
 
-  letsPlay(): void{
-    
+  letsPlay(): void {
+    console.log(this.playersArray);
   }
 
-
-  constructor(private coursesService: CoursesService) { }
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit(): void {}
-
 }
