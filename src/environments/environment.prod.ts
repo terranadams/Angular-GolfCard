96ecc8cffce9af9e2a3ac619e5806b833d823894
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-02-01 12:04:42 -0700

    made changes to environment so firebase would work when running build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBDoofhNOzoJcYHg0AIKvQDtxaiXV6mssI',
+    authDomain: 'golfscore-app-ad248.firebaseapp.com',
+    projectId: 'golfscore-app-ad248',
+    storageBucket: 'golfscore-app-ad248.appspot.com',
+    messagingSenderId: '891988601760',
+    appId: '1:891988601760:web:6a093b2036a1a7e14492c5',
+  },
 };
